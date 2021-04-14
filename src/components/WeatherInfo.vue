5ee0ad7aca03e6f9cd30036eebb4bb5eb775f11a
jsking216 <jarf1337@gmail.com>
2021-04-13 23:00:53 -0400

    add a geolocation component

---
+++ src/components/WeatherInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <span v-if="errorStr === null">Your location is {{ JSON.stringify(location) }}</span>
+    <span v-else>{{ errorStr }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DemoComponent",
+  data:{
+    location:null,
+    errorStr:null
+  },
+  created() {
+    this.$getLocation()
+      .then((coordinates) => {
+        this.location = coordinates;
+        console.log(coordinates);
+      })
+      .catch((error) => {
+        this.errorStr = error.message;
+        console.log(error);
+      });
+  },
+};
+</script>
