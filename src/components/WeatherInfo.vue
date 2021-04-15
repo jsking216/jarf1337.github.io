6b1a2016377c5e348d0926cb8ca2d449d6d0baa2
jsking216 <jarf1337@gmail.com>
2021-04-14 23:19:22 -0400

    testing weather api call

--- src/components/WeatherInfo.vue
+++ src/components/WeatherInfo.vue
@@ -2,28 +2,37 @@
   <div>
     <span v-if="errorStr === null">Your location is {{ JSON.stringify(location) }}</span>
     <span v-else>{{ errorStr }}</span>
+    <span v-if="errorStr === null">Your weather is {{ JSON.stringify(weather) }}</span>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "DemoComponent",
   data: function() {
     return {
     location:null,
-    errorStr:null
+    errorStr:null,
+    weather: null,
   };
-},
-  created() {
+  },
+  mounted () {
     this.$getLocation()
       .then((coordinates) => {
         this.location = coordinates;
         console.log(coordinates);
+        const callUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lng}&appid=2b83c216854254bdc36a1293fe363579`;
+        console.log(callUrl);
+        return axios.get(callUrl);
       })
+      .then(response => (this.weather = response))
       .catch((error) => {
         this.errorStr = error.message;
         console.log(error);
       });
+    
   },
 };
 </script>
